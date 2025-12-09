c7c791ff7ad005e22073cc9207680c9e92cb88e9
niels <Niels.versteeg03@gmail.com>
2025-12-09 17:08:28 +0100

    AHHHHH

--- ImprovedCresi/main.py
+++ ImprovedCresi/main.py
@@ -1,34 +1,15 @@
 import argparse
-import os
 
+import numpy as np
+import s3fs
 import torch
 from PIL import Image
-from diffusionsat.pipeline_controlnet import StableDiffusionControlNetPipeline
-from diffusionsat.data_util import metadata_normalize
-from diffusionsat import DiffusionSatControlNetPipeline, DiffusionSatPipeline, SatUNet
-from diffusionsat.pipeline import StableDiffusionPipeline
-from diffusionsat.controlnet import ControlNetModel
 from src.diffusers.pipelines import StableDiffusionInpaintPipeline
+from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
+
+
 def argparser():
     parser = argparse.ArgumentParser(description="InpaintCresi Command Line Interface")
-    parser.add_argument(
-        "--unet-model-path",
-        type=str,
-        default="finetune_sd21_sn-satlas-fmow_snr5_md7norm_bs64/",
-        help="Path to the UNet model checkpoint",
-    )
-    parser.add_argument(
-        "--ctrlnet-model-path",
-        type=str,
-        default="controlnet_sd21_md7norm_fmow_condres256",
-        help="Path to the ControlNet model checkpoint",
-    )
-    parser.add_argument(
-        "--data",
-        type=str,
-        default=None,
-        help="Input data for processing",
-    )
     parser.add_argument(
         "--img-size-x",
         type=int,
@@ -48,82 +29,110 @@ def argparser():
         help="Number of inference steps for the diffusion process",
     )
     parser.add_argument(
-        "--image",
+        "--image-s3",
         type=str,
         default="example_data/satellite_image.png",
         help="Path to the input image for inpainting",
-    )
-    parser.add_argument(
-        "--mask",
-        type=str,
-        default="example_data/mask.png",
-        help="Path to the input mask for inpainting",
     )
     return parser.parse_args()
 
 
 class InpaintCresi:
-    def __init__(self, unet_checkpoint_path: str, ctrlnet_checkpoint_path: str, img_size_x: int, img_size_y: int, num_inference_steps: int) -> None:
-        self.unet_checkpoint_path = unet_checkpoint_path
-        self.ctrlnet_checkpoint_path = ctrlnet_checkpoint_path
-        
+    def __init__(
+        self,
+        fs: s3fs.S3FileSystem,
+        img_size_x: int,
+        img_size_y: int,
+        num_inference_steps: int,
+    ) -> None:
+        self.fs = fs
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Using device: {self.device}")
-        
+
         self.img_size_x = img_size_x
-        self.img_size_y = img_size_y    
+        self.img_size_y = img_size_y
         self.num_inference_steps = num_inference_steps
         self.guidance_scale = 7.5
 
-    def SatUNet_pipeline(self) -> StableDiffusionControlNetPipeline:
-        # unet = SatUNet.from_pretrained(
-        #     self.unet_checkpoint_path + "checkpoint-150000",
-        #     subfolder="unet",
-        #     num_metadata=7,
-        # )
-        # controlnet = ControlNetModel.from_pretrained(
-        #     self.ctrlnet_checkpoint_path,
-        #     subfolder="controlnet",
-        #     num_metadata=7,
-        #     torch_dtype=torch.float16
-        # )
-        pipe = StableDiffusionInpaintPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-inpainting", cache_dir="/local/s3322637/.cache/").to(self.device)
-        return pipe
+        self.feature_extractor = SegformerFeatureExtractor.from_pretrained(
+            "Burdenthrive/cloud-detection-segformer-mit_b4-RGB",
+            cache_dir="/local/s3322637/.cache/",
+        )
+        self.segformer_model = (
+            SegformerForSemanticSegmentation.from_pretrained(
+                "Burdenthrive/cloud-detection-segformer-mit_b4-RGB",
+                cache_dir="/local/s3322637/.cache/",
+            )
+            .to(self.device)
+            .eval()
+        )
 
-    def inpaint(self, image_path: str, mask_path: str):
-        pipe = self.SatUNet_pipeline()
+        self.inpaint_pipe = StableDiffusionInpaintPipeline.from_pretrained(
+            "stable-diffusion-v1-5/stable-diffusion-inpainting"
+        ).to(self.device)
 
-        init_image = Image.open(image_path).convert("RGB").resize((self.img_size_x, self.img_size_y))
-        mask_image = Image.open(mask_path).convert("L").resize((self.img_size_x, self.img_size_y))
+        print(type(self.inpaint_pipe))
         
-        caption = "a satellite image of a city with buildings and roads inpainted realistically"
-        # metadata: [longitude, latitude, gsd, cloud cover, year, month, day]
-        # metadata = metadata_normalize([76.5712666476, 28.6965307997, 0.929417550564, 0.0765712666476, 2015, 2, 27]).tolist()
+    def load_s3_image(self, s3_path: str) -> Image.Image:
+        with self.fs.open(s3_path, "rb") as f:
+            img = Image.open(f).convert("RGB")
+        img = img.resize((self.img_size_x, self.img_size_y))
+        return img
 
-        output = pipe(
-            prompt=caption,
-            image=init_image,
-            mask_image=mask_image,
+    def detect_cloud_mask(self, image: Image.Image) -> Image.Image:
+        # Preprocess
+        inputs = self.feature_extractor(images=image, return_tensors="pt").to(
+            self.device
+        )
+        with torch.no_grad():
+            outputs = self.segformer_model(**inputs)
+
+        logits = outputs.logits
+        mask = torch.argmax(logits, dim=1)[0].cpu().numpy()
+
+        cloud_mask = ((mask == 1) | (mask == 2)).astype(np.uint8) * 255
+        return Image.fromarray(cloud_mask).resize((self.img_size_x, self.img_size_y))
+
+    def inpaint(
+        self, image: Image.Image, mask: Image.Image, prompt: str, output_path: str
+    ):
+        result = self.inpaint_pipe(
+            prompt=prompt,
+            image=image,
+            mask_image=mask,
             num_inference_steps=self.num_inference_steps,
             guidance_scale=self.guidance_scale,
             height=self.img_size_y,
             width=self.img_size_x,
         ).images[0]
-
-        output.save("inpainted_image.png")
+        result.save(output_path)
+        print(f"Inpainted image saved to {output_path}")
 
     def cresi(self, data):
         pass
 
 
 def main(args: argparse.Namespace):
-    processor = InpaintCresi(args.unet_model_path, args.ctrlnet_model_path, args.img_size_x, args.img_size_y, args.num_inference_steps)
-    output = processor.inpaint(args.image, args.mask)
+    fs = s3fs.S3FileSystem(anon=True)
+    print(
+        fs.ls(
+            "s3://spacenet-dataset/spacenet/SN5_roads/test_public/AOI_8_Mumbai/PS-RGB/"
+        )
+    )
+    processor = InpaintCresi(
+        fs,
+        args.img_size_x,
+        args.img_size_y,
+        args.num_inference_steps,
+    )
+    img = processor.load_s3_image(args.image_s3)
+    mask = processor.detect_cloud_mask(img)
+
+    prompt = "A satellite image of a city with buildings and roads, clouds removed realistically"
+    output = processor.inpaint(img, mask, prompt, "inpainted_image.png")
     output = processor.cresi(output)
 
 
 if __name__ == "__main__":
-    os.environ["HF_HOME"] = "/local/s3322637/.cache/"
-
     args = argparser()
     main(args)
