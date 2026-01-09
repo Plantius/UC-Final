1573a05d35ff7b8d7e2ef323eda677d06ca54f70
niels <Niels.versteeg03@gmail.com>
2026-01-09 16:20:23 +0100

    a

--- ImprovedCresi/main.py
+++ ImprovedCresi/main.py
@@ -230,8 +230,8 @@ class InpaintCresi:
     def inpaint(
         self, image: Image.Image, mask: Image.Image, prompt: str, output_path: str
     ):
-        padded_image, original_size = pad_to_multiple(image, 512, fill=0)
-        padded_mask, _ = pad_to_multiple(mask, 512, fill=0)
+        padded_image, original_size = pad_to_multiple(image, 1024, fill=0)
+        padded_mask, _ = pad_to_multiple(mask, 1024, fill=0)
 
         padded_result = self.inpaint_full_image(
             image=padded_image,
