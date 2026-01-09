bc5d87e1acf518705b9d5a7ce5a3950aa3fb8baf
niels <Niels.versteeg03@gmail.com>
2026-01-09 17:23:19 +0100

    New model

--- ImprovedCresi/main.py
+++ ImprovedCresi/main.py
@@ -121,7 +121,6 @@ class InpaintCresi:
                 self.inpaint_model_name,
                 cache_dir=f"/local/{self.username}/.cache/",
                 torch_dtype=torch.float16,
-                variant="fp16",
             ).to(self.device)
 
             self.inpaint_pipe.enable_model_cpu_offload()
